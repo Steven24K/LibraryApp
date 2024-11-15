9618b13879be20d70ac214b857a14a26c5d2df6c
Steven <s.koerts2@gmail.com>
2024-11-15 16:03:38 +0000

    Add code

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # LibraryApp
+
+This application uses the bare minimum setup for a React App using Webpack and Typescript. 
+
+## Running 
+
+`yarn install`
+
+`yarn watch`
+
+## Sample case
+
+Imagine you’re working on a college library search system that helps students find books based on specific criteria. The system has a large list of books, and you want to let students filter books based on multiple criteria, such as genre, title, year published, and author.
+However, you want each criteria to be applied independently so that students can start with one criteria and add others as needed. 
+
+### Exercise 1 
+Continue to implement your own linked list structure that can be used to render
+the list of books in the state. 
+Make sure the list has the following methods implemented: 
+- reduce 
+- map 
+- toArray
+- count
+- filter
+
+### Exercise 2
+Applying the concept of currying, build a modular filter system where you can select 
+an attribute from a dropdown and apply the content from the search box on the list of 
+books. Passing 1 argument at the time. 
+
+### Exercise 3
+Using the concept of function composition, make it possible to apply more than 1 filter. 
+Do not change the concept of currying from the previous question. 
+
---
+++ src/App.tsx
@@ -0,0 +1,84 @@
+import React = require("react");
+import '../static/css/site.css'
+
+type Book = {
+    id: number
+    title: string
+    author: string
+    genre: string
+    year: number
+}
+
+type BookProps = Exclude<keyof Book, "id"> //type BookProps =  "title" | "author" | "author" | "year"
+// See: https://www.typescriptlang.org/docs/handbook/2/keyof-types.html 
+
+type Library = Book[]
+
+type Func<a, b> = (_: a) => b
+
+type BookPredicate<FilterKey extends BookProps> = Func<Book[FilterKey], boolean> // i.e. title => title == "Advanced Types in Typescript"
+
+type BookFilter = Func<Book, BookPredicate<BookProps>> // i.e. book => title => book.title == title
+
+type LibraryFilter = Func<Library, Func<BookFilter, Library>> // i.e. books => book => title => book.title == title
+
+interface AppProps {
+
+}
+
+interface AppState {
+    library: Library
+    search: string | number
+    selectedFilter: BookProps
+}
+
+export class App extends React.Component<AppProps, AppState> {
+    constructor(props: AppProps) {
+        super(props)
+        this.state = {
+            library: [
+                { id: 1, title: "The Pragmatic Programmer", author: "Andrew Hunt", genre: "Technology", year: 1999 },
+                { id: 2, title: "Clean Code", author: "Robert C. Martin", genre: "Technology", year: 2008 },
+                { id: 3, title: "To Kill a Mockingbird", author: "Harper Lee", genre: "Fiction", year: 1960 },
+                { id: 4, title: "1984", author: "George Orwell", genre: "Fiction", year: 1949 },
+                { id: 5, title: "Sapiens", author: "Yuval Noah Harari", genre: "Non-Fiction", year: 2011 },
+            ],
+            search: "",
+            selectedFilter: "title"
+        }
+    }
+
+    filterAction: LibraryFilter = (library: Library) => (bookFilter: BookFilter) => library.filter(bookFilter)
+
+    render(): React.ReactNode {
+        return <div className="main">
+            <h1>Welcome to the library</h1>
+            <div className="filters">
+                <label>Filter on:</label>
+                <select defaultValue={this.state.selectedFilter}>
+                    <option value="title">Title</option>
+                    <option value="author">Auhtor</option>
+                    <option value="genre">Genre</option>
+                    <option value="year">Year</option>
+                </select>
+                <input type="text" name="search" value={this.state.search} placeholder={`Search books on ${this.state.selectedFilter}`} />
+                <button disabled={this.state.search == ""}>Search</button>
+            </div>
+
+            <h2>{this.state.library.length} books found</h2>
+
+            <ul className="book-list">
+                {
+                    this.state.library.map(book =>
+                        <li key={book.id}>
+                            <h2>{book.title} </h2>
+                            <p className="author">Author: {book.author}</p>
+                            <p className="genre">Genre: {book.genre}</p>
+                            <p className="published">Published in: {book.year}</p>
+                        </li>
+                    )
+                }
+            </ul>
+        </div>
+    }
+}
