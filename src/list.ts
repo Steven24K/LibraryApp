e771bd0214f4a4717a33cd8d9be8bc37853d3680
ErikTanis <eriktanis21@gmail.com>
2024-11-20 13:36:22 +0100

    Added count & filter method

--- src/list.ts
+++ src/list.ts
@@ -8,15 +8,43 @@ type EmptyNode<a> = {
     kind: 'empty'
 }
 
-export type List<a> = (ListNode<a> | EmptyNode<a>)
+type ListMethods<a> = {
+    reduce: <b>(this: List<a>, f: (acc: b, curr: a) => b, initial: b) => b
+    map: <b>(f: (v: a) => b) => List<b>
+    toArray: () => a[]
+    count: () => number
+    filter: (predicate: (x: a) => boolean) => List<a>
+}
 
-export const ListNode = <a>(_v: a, _tail: List<a>): ListNode<a> => ({
-    kind: 'node',
-    value: _v,
-    tail: _tail
+export type List<a> = (ListNode<a> | EmptyNode<a>) & ListMethods<a>
+
+const ListMethods = <a>(): ListMethods<a> => ({
+    reduce: function<b>(this: List<a>, f: (acc: b, v: a) => b, init: b): b {
+        if (this.kind == 'empty') return init
+        return this.tail.reduce(f, f(init, this.value))
+    },
+    map: function<b>(this: List<a>, f: (v: a) => b): List<b> {
+        return this.reduce((acc, v) => ListNode(f(v), acc), EmptyNode())
+    },
+    toArray: function(this: List<a>): a[] {
+        return this.reduce((acc, v) => acc.concat([v]), Array<a>())
+    },
+    filter: function(this: List<a>, predicate: (x: a) => boolean): List<a> {
+        return this.reduce((acc, v) => predicate(v) ? ListNode(v, acc) : acc, EmptyNode())
+    },
+    count: function(this: List<a>): number {
+        return this.reduce((acc, _) => acc + 1, 0)
+    }
 })
 
-export const EmptyNode = <a>(): EmptyNode<a> => ({ kind: 'empty' })
+export const ListNode = <a>(_v: a, _tail: List<a>): List<a> => ({
+    kind: 'node',
+    value: _v,
+    tail: _tail,
+    ...ListMethods<a>()
+})
 
-// i.e. 
-const list1: List<number> = ListNode(1, ListNode(2, ListNode(3, ListNode( 4, ListNode(5, EmptyNode())))))
+export const EmptyNode = <a>(): List<a> => ({
+    kind: 'empty',
+    ...ListMethods<a>()
+})
