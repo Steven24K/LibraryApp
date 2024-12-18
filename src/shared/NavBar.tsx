cd9ae24e71db39b2567b74f0fd93c12399c4b7b9
ErikTanis <eriktanis21@gmail.com>
2024-12-18 13:45:49 +0100

    Cleaned up navbar

--- src/shared/NavBar.tsx
+++ src/shared/NavBar.tsx
@@ -6,10 +6,6 @@ export const NavBar = () => {
             <li><NavLink to="/">Home</NavLink></li>
             <li><NavLink to="/about">About</NavLink></li>
             <li><NavLink to="/contact">Contact</NavLink></li>
-
-            <li><a href="/">Home a </a></li>
-            <li><a href="/about">About a</a></li>
-            <li><a href="/contact">Contact a</a></li>
         </ul>
     </nav>
 }
